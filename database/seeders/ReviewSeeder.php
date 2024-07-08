2d505121ddd8368d1c75a2f049db7cf781235434
InnShh <b06032011@gmail.com>
2024-07-08 22:03:37 +0200

    making reviews structure and seeder

---
+++ database/seeders/ReviewSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Review;
+
+class ReviewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Review::factory()->count(3)->create();
+        Review::create([
+            'pseudo' => 'Sarah',
+            'comment' => 'I had a fantastic experience at Arcadia Zoo! The enclosures are spacious and beautifully designed, and it\'s clear the animals are well cared for. The staff is knowledgeable and friendly, making the visit even more enjoyable. I especially loved the Savanna exhibit with the giraffes and lions. Definitely worth to visit!',
+            'rating' => 3,
+            'created_at' => '2024-01-20 00:00:00',
+            'approved' => false,
+        ]);
+
+        Review::create([
+            'pseudo' => 'John',
+            'comment' => 'I had a fantastic experience at Arcadia Zoo! The enclosures are spacious and beautifully designed, and it\'s clear the animals are well cared for. The staff is knowledgeable and friendly, making the visit even more enjoyable. I especially loved the Savanna exhibit with the giraffes and lions. Definitely worth to visit!',
+            'rating' => 4,
+            'created_at' => '2023-11-05 00:00:00',
+            'approved' => false,
+        ]);
+    }
+}
