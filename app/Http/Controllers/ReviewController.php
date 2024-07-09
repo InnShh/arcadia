ff17c467f38511a8865062187b61ab816e550593
InnShh <b06032011@gmail.com>
2024-07-09 18:44:21 +0200

    storing reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    function store(Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'message' => 'required|string',
+            'rating' => 'required|integer|min:1|max:5',
+        ]);
+
+        Review::create([
+            'pseudo' => $validatedData['name'],
+            'comment' => $validatedData['message'],
+            'rating' => $validatedData['rating'],
+            'approved' => false, // Default to false for moderation
+        ]);
+
+        return response()->json(['success' => true]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', [HomePageController::class, 'index'])->name('homepage');
 Route::get('/savanna', fn () => view('savannapage'))->name('savanna');
 Route::get('/savanna/giraffe-max', fn () => view('giraffemaxpage'))->name('giraffemax');
 Route::get('/forest', fn () => view('forestpage'))->name('forest');
 Route::get('/arctic', fn () => view('arcticpage'))->name('arctic');
-
+Route::post('/reviews/store', [ReviewController::class, 'store'])->name('review.store');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
