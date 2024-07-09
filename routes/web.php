0d8db4b8f657eefe00c2d430475cbcc44dd57f0c
InnShh <b06032011@gmail.com>
2024-07-09 22:08:05 +0200

    modifying to look like resource controller

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/savanna', fn () => view('savannapage'))->name('savanna');
 Route::get('/savanna/giraffe-max', fn () => view('giraffemaxpage'))->name('giraffemax');
 Route::get('/forest', fn () => view('forestpage'))->name('forest');
 Route::get('/arctic', fn () => view('arcticpage'))->name('arctic');
-Route::post('/reviews/store', [ReviewController::class, 'store'])->name('review.store');
+Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
