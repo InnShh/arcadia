066c2c855320cc0e8b50b6d32dc22f0d51d40655
InnShh <b06032011@gmail.com>
2024-07-11 10:42:24 +0200

    initialize the login page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -16,9 +16,19 @@
             <a href="{{ url('/') }}#reviews-all">Reviews</a>
         </li>
 
-        {{--    <li>
-            <a href="">Log In</a>
-        </li> --}}
+        <li>
+        @if (Route::has('login'))
+            @auth
+                <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                    Dashboard
+                </a>
+            @else
+                <a href="{{ route('login') }}" class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                    Log in
+                </a>
+            @endauth
+        @endif
+        </li>
 
         <li>
             <a href="">Terms & Conditions</a>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -34,7 +34,7 @@
                             <li>
                                 <a href="{{ url('/') }}#contact-all">contact</a>
                             </li>
-                        {{--    <li>
+                            <li>
                                 @if (Route::has('login'))
                                     @auth
                                     <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
@@ -46,7 +46,7 @@
                                     </a>
                                     @endauth
                                 @endif
-                            </li> --}}
+                            </li>
                         </ul>
                     </div>
                 </div>
