b2d349e95704411c969df9645bd7a9bc116f57ae
InnShh <b06032011@gmail.com>
2024-07-11 21:29:16 +0200

    passing animals and exhibits to homepage

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Review;
 use App\Models\Activity;
+use App\Models\Animal;
+use App\Models\Exhibit;
 
 class HomePageController extends Controller
 {
     function index()
     {
+        $animals = Animal::with(['images', 'exhibit'])->get();
+        $exhibits = Exhibit::with(['images'])->get();
         $reviews = Review::where('approved', True)->latest()->take(13)->get();
         $activities = Activity::all();
-        return view('homepage', compact('reviews', 'activities'));
+        return view('homepage', compact(
+            'reviews',
+            'activities',
+            'exhibits',
+            'animals',
+        ));
     }
 }
