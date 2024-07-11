fee48f799edc30b56969e378b3f019c0274a06ae
InnShh <b06032011@gmail.com>
2024-07-11 21:28:49 +0200

    adding animal exhibit relation

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Animal extends Model
@@ -12,5 +13,9 @@ class Animal extends Model
     public function images(): HasMany
     {
         return $this->hasMany(AnimalImage::class, 'animal_id');
+    }
+    public function exhibit(): BelongsTo
+    {
+        return $this->belongsTo(Exhibit::class, 'exhibit_id');
     }
 }
