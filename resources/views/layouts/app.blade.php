f185ee1b420c75c11b405092d9f2d859d94991ff
InnShh <b06032011@gmail.com>
2024-07-11 12:03:36 +0200

    styling the login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,13 @@
 <body class="dashboard">
     <div id="app">
         <nav class="navbar navbar-expand-md">
-            <div class="container">
+            <div class="container justify-content-end">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'ARCADIA ZOO') }}
+                    <img src="/images/arcadia_logo.svg" alt="Arcadia logo">
                 </a>
+                {{--
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -35,7 +36,7 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                        {{--
+
                         @if (Route::has('login'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -46,7 +47,7 @@
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                         @endif
-                        --}}
+
                         @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -66,7 +67,7 @@
                         </li>
                         @endguest
                     </ul>
-                </div>
+                </div> --}}
             </div>
         </nav>
 
