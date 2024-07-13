5ed162ac3f094c5953767b59328204abe151799d
InnShh <b06032011@gmail.com>
2024-07-13 18:40:30 +0200

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,7 @@ Route::get('/', [HomePageController::class, 'index'])->name('homepage');
 Route::post('/load-more-animals', [HomePageController::class, 'loadMoreAnimals'])->name('homepage.load-more-animals');
 Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
 Auth::routes();
-Route::get('/{exhibit:slug}', [ExhibitController::class, 'show'])->name('exhibits.show');
-Route::get('/savanna/giraffe-max', fn () => view('giraffemaxpage'))->name('giraffemax');
 Route::get('/{exhibit:slug}/{animal:slug}', [AnimalController::class, 'show'])->name('animals.show');
+Route::get('/{exhibit:slug}', [ExhibitController::class, 'show'])->name('exhibits.show');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
