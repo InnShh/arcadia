8f722f4590039862ee1c2cc5e82c50fcab88846f
InnShh <b06032011@gmail.com>
2024-07-20 21:42:27 +0200

    fixing if no reviews pending

--- resources/views/emp/reviews/index.blade.php
+++ resources/views/emp/reviews/index.blade.php
@@ -7,9 +7,15 @@
                 {{ session('success') }}
             </div>
             @endif
+            @if($reviews->isEmpty())
+            <div class="alert alert-info">
+                No pending reviews.
+            </div>
+            @else
             @foreach ($reviews as $review)
             <x-review-card :review="$review" />
             @endforeach
+            @endif
         </div>
     </div>
 </x-app-layout>
