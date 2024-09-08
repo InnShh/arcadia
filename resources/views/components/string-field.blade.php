f7508d84e6263e7189f316b4d30139c868189714
InnShh <b06032011@gmail.com>
2024-09-08 18:46:00 +0200

    fix validation errors output for string fields

--- resources/views/components/string-field.blade.php
+++ resources/views/components/string-field.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <label for="{{$name}}">{{$label}}:</label>
     <input type="$type" name="{{$name}}" id="{{$name}}" class="form-control @error($name)is-invalid @enderror" value="{{ old($name,$value??'') }}" @required($required??false)>
-    @error('name')
+    @error($name)
     <span class="invalid-feedback" role="alert">
         {{ $message }}
     </span>
