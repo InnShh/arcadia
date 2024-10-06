841e06005b5c5c9fbe6dcc908ac43e05927e7631
InnShh <b06032011@gmail.com>
2024-10-06 15:03:17 +0200

    deploy from main and run build after npm install

--- deploy.php
+++ deploy.php
@@ -8,6 +8,7 @@ require 'contrib/npm.php';
 // Config
 
 set('repository', 'git@github.com:InnShh/arcadia.git');
+set('branch', 'main');
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -19,7 +20,14 @@ host('innash.net')
     ->set('remote_user', 'deployer')
     ->set('deploy_path', '~/arcadia');
 
+// Tasks
+
+task('npm:build', function () {
+    run('cd {{release_path}} && npm run build');
+});
+
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
 after('deploy:update_code', 'npm:install');
+after('npm:install', 'npm:build');
